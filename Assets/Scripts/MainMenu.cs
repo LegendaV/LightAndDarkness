ae7c53eb2572a107540af1e1760e44c766fdccd7
LegendaV <115637903+LegendaV@users.noreply.github.com>
2023-05-04 21:05:45 +0500

    Load without enemy

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -12,6 +13,29 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(_startGameScene.name);
+    }
+
+    public void LoadGame()
+    {
+        var asyncOp = SceneManager.LoadSceneAsync(_startGameScene.name, LoadSceneMode.Single);
+        asyncOp.allowSceneActivation = false;
+        while (!asyncOp.isDone)
+        {
+            if (asyncOp.progress >= 0.9f)
+            {
+                break;
+            }
+        }
+        asyncOp.allowSceneActivation = true;
+        asyncOp.completed += operation =>
+        {
+            var nextScene = SceneManager.GetSceneByName(_startGameScene.name);
+            var all = FindObjectsOfType<EnemySpawner>();
+            foreach (var gameObject in all)
+            {
+                gameObject.SetNotAlive();
+            }
+        };
     }
 
     public void ExitGame()
